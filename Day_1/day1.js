f666612dd5bd01ab3810f381fbee3efe77696ec4
Erika Green <erikagreen7777@gmail.com>
2024-12-21 12:23:27 -0500

    day1

---
+++ Day_1/day1.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const { type } = require('os');
+
+const input = fs.readFileSync('./input.txt', 'utf-8'); 
+let leftList = [];
+let rightList = [];
+let differenceArray = [];
+
+const lines = input.split('\n'); 
+
+// Separate columns
+for (let i = 0; i< lines.length; i++) {
+    let one_line = Array.from(lines[i].split(/\s/));
+    let end = one_line.length-1
+    leftList = [...leftList, one_line[0]];
+    rightList = [...rightList, one_line[end]];
+}
+
+leftList = leftList.sort();
+rightList = rightList.sort();
+
+
+// Create array of element differences
+for (let i = 0; i < lines.length ; i++) {
+    let greaterNumber = leftList[i] >= rightList[i] ? leftList[i] : rightList[i];
+    let smallerNumber = leftList[i] < rightList[i] ? leftList[i] : rightList[i]; 
+    let difference = greaterNumber - smallerNumber;
+    differenceArray = [...differenceArray, difference]
+}
+
+// Sum element differences array
+const total_difference = differenceArray.reduce((acc, curr) => acc + curr)
+
+console.log(total_difference)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # adventOfCode2024
-Advent of Code is an Advent calendar of small programming puzzles for a variety of skill levels that can be solved in any programming language you like.
+Advent of Code(https://adventofcode.com/) is an Advent calendar of small programming puzzles for a variety of skill levels that can be solved in any programming language you like.
