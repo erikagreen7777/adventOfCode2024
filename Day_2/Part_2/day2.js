e02a2eccdaaa7299450f92268a3e08189ed3e073
Erika Green <erikagreen7777@gmail.com>
2024-12-29 09:02:43 +0100

    need to run diffchecker earlier

--- Day_2/Part_2/day2.js
+++ Day_2/Part_2/day2.js
@@ -1,106 +1,120 @@
-const fs = require('fs');
-
-const input = fs.readFileSync('./input.txt', 'utf-8');
-
-const lines = input.split('\n');
-let safeCounter = 0;
-
 function isDesc(arr) {
-    const n = arr.length;
-    for (let i = 1; i < n; i++) {
-        if (arr[i - 1] <= arr[i]) {
-            return false;
-        }
+  const n = arr.length;
+  for (let i = 1; i < n; i++) {
+    if (arr[i - 1] <= arr[i]) {
+      return false;
     }
-    console.log(`isDesc yes: ${arr}`);
-    return true;
+  }
+  return true;
 }
 
 function isAsc(arr) {
-    const n = arr.length;
-    for (let i = 1; i < n; i++) {
-        if (arr[i - 1] >= arr[i]) {
-            return false;
-        }
+  const n = arr.length;
+  for (let i = 1; i < n; i++) {
+    if (arr[i - 1] >= arr[i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 function problemDampener(arr, funcToTest) {
-    let result;
-    let testArr;
-    for (let i = 0; i < arr.length; i++) {
-        testArr = arr.toSpliced(i, 1);
-        result = {
-            value: funcToTest(testArr),
-            element: i
-        };
-        // Object.keys(result).forEach(key => {
-        //     console.log(`hey: ${key}: ${result[key]}`);
-        //     console.log(`result.value: ${result.value}`);
-        // });
-        if (result.value === true) {
-            // console.log(`true`);
-            break;
-        }
+  let result = {
+    value: false,
+    element: -1,
+  };
+  let testArr;
+  for (let i = 0; i < arr.length; i++) {
+    testArr = arr.toSpliced(i, 1);
+    result = {
+      value: funcToTest(testArr),
+      element: i,
+    };
+    if (result.value === true) {
+      break;
     }
+  }
 
-    return result;
+  return result;
 }
 
-/* IT'S HERE - THE ISSUE'S HERE WITH RESULT */
 function isOrdered(arr) {
-    let isAscend = isAsc(arr);
-    // console.log(`❤️: ${isAscend}`);
-    let isDescend = isDesc(arr);
-    let result = { value: false, element: -1 }; ;
+  // let isAscend = isAsc(arr);
+  // let isDescend = isDesc(arr);
+  let isAscend = problemDampener(arr, isAsc);
+  let isDescend = problemDampener(arr, isDesc);
+  let result = {
+    value: false,
+    element: -1,
+  };
 
-    console.log(`original isAscend result: ${isAscend}`);
-    if (!isAscend) {
-        result = problemDampener(arr, isAsc);
+  // RUN DIFF CHECKER HERE
+  let eitherSorted = isAscend || isDescend;
 
-    } else if (!isDescend) {
-        result = problemDampener(arr, isDesc)
-    }
-    Object.keys(result).forEach(key => {
-        console.log(`🤔: ${key}: ${key[result]}`)
+  return eitherSorted;
+  // if (eitherSorted) {
+  //   result = {
+  //     value: true,
+  //     element: -1,
+  //   };
+  //   return result;
+  // }
 
-    })
-    return result;
+  // if (!isAscend) {
+  //   result = problemDampener(arr, isAsc);
+  // }
+  // if (result.value === false) {
+  //   result = problemDampener(arr, isDesc);
+  // }
+  // if (!isDescend) {
+  //   result = problemDampener(arr, isDesc);
+  // }
+  // return result;
 }
 
 function diffChecker(numArray) {
-    for (let i = 1; i < numArray.length; i++) {
-        const diff = Math.abs(numArray[i] - numArray[i - 1]);
-        if (diff > 3) {
-            bool = false;
-            break;
-        } else {
-            bool = true;
-        }
+  let bool;
+  for (let i = 1; i < numArray.length; i++) {
+    const diff = Math.abs(numArray[i] - numArray[i - 1]);
+    if (diff > 3) {
+      bool = false;
+      break;
+    } else {
+      bool = true;
     }
-    return bool;
+  }
+  return bool;
 }
 
 function isSafe(arr) {
-    let numArray = arr.map(Number);
-    let bool = false;
-    let result = isOrdered(numArray);
-    // Object.keys(result).forEach(key => {
-    //     console.log(`isSafe: ${key}: ${result[key]}`)
-    // })
-    if (result === true) {
-        bool = diffChecker(numArray);
-    }
-    return bool;
+  let numArray = arr.map(Number);
+  let bool;
+  let result = isOrdered(numArray);
+  let newArray;
+
+  if (result.value === true) {
+    newArray =
+      result.element > -1 ? numArray.toSpliced(result.element, 1) : numArray;
+    // bool = diffChecker(newArray); <--- FIGURE OUT WHAT TO DO WITH THIS
+  } else {
+    bool = false;
+  }
+  return bool;
 }
 
-for (let i = 3; i < 4 /* lines.length */; i++) {
-    let one_line = Array.from(lines[i].split(' '));
+function main() {
+  const fs = require("fs");
+  const input = fs.readFileSync("./input.txt", "utf-8");
+  const lines = input.split("\n");
+  let safeCounter = 0;
+  for (let i = 0; i < 3 /*lines.length*/; i++) {
+    let one_line = Array.from(lines[i].split(" "));
     if (isSafe(one_line)) {
-        console.log(`main: ${one_line}`);
-        safeCounter += 1;
-        console.log(`current safe count: ${safeCounter}`);
+      safeCounter += 1;
     }
+  }
+  console.log(safeCounter);
+  return safeCounter;
 }
-console.log(safeCounter);
+
+main();
