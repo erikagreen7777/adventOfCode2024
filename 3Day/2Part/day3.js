3be81d221739ee6bb548ac8aa57174a9844ead07
Erika Green <erikagreen7777@gmail.com>
2025-01-02 18:39:09 +0100

    get indexes of commands

--- 3Day/2Part/day3.js
+++ 3Day/2Part/day3.js
@@ -9,21 +9,37 @@ function main() {
   const lines = initialDo.split("\n");
 
   for (let i = 0; i < lines.length; i++) {
+    const doIndexes = [];
+    const dontIndexes = [];
     const one_line = lines[i];
-    const matches = [...one_line.matchAll(nums)];
     const dosMatches = [...one_line.matchAll(dos)];
     const dontsMatches = [...one_line.matchAll(donts)];
-    for (let j = 0; j < matches.length; j++) {
-      const match = matches[j][0];
-      const doIndexes = dosMatches[i][1];
-      console.log(doIndexes);
-      const num1 = match.split("(")[1].split(",")[0];
-      const num2 = match.split("(")[1].split(",")[1].split(")")[0];
-      const prod = num1 * num2;
-      total += prod;
-    }
+    dosMatches.forEach((match) => {
+      doIndexes.push(match.index);
+    });
+    dontsMatches.forEach((match) => {
+      dontIndexes.push(match.index);
+    });
+    // console.log(dosMatches[i].index);
+    console.log(doIndexes + "\n" + dontIndexes);
   }
   console.log(total);
+  //   for (let i = 0; i < lines.length; i++) {
+  //     const one_line = lines[i];
+  //     const matches = [...one_line.matchAll(nums)];
+  //     const dosMatches = [...one_line.matchAll(dos)];
+  //     console.log(dosMatches[i]);
+
+  //     // const dontsMatches = [...one_line.matchAll(donts)];
+  //     for (let j = 0; j < matches.length; j++) {
+  //       const match = matches[j][0];
+  //       const num1 = match.split("(")[1].split(",")[0];
+  //       const num2 = match.split("(")[1].split(",")[1].split(")")[0];
+  //       const prod = num1 * num2;
+  //       total += prod;
+  //     }
+  //   }
+  //   console.log(total);
 }
 
 main();
